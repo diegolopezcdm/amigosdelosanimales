802f9ebebae87aea74d4e3a2439ed4c3377b1aa9
cesar <lvalencia1286@gmail.com>
2015-12-05 00:56:40 -0500

    añadidos cambios

--- ui/mobile/Adoptapet/app/src/main/java/pe/adoptapet/adoptapet/model/Constants.java
+++ ui/mobile/Adoptapet/app/src/main/java/pe/adoptapet/adoptapet/model/Constants.java
@@ -25,15 +25,10 @@ public class Constants {
 
 
     public static Bitmap cropToSquare(Bitmap bitmap){
-
         int width  = bitmap.getWidth();
-
         int height = bitmap.getHeight();
-
         int newWidth = (height > width) ? width : height;
-
         int newHeight = (height > width)? height - ( height - width) : height;
-
         int cropW = (width - height) / 2;
         cropW = (cropW < 0)? 0: cropW;
         int cropH = (height - width) / 2;
@@ -79,9 +74,11 @@ public class Constants {
         paint.setAntiAlias(true);
         canvas.drawARGB(0, 0, 0, 0);
         paint.setColor(color);
+
         // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
         canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
                 bitmap.getWidth() / 2, paint);
+
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
         canvas.drawBitmap(bitmap, rect, rect, paint);
         //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
@@ -94,24 +91,34 @@ public class Constants {
 
 
             if (requestCode == Constants.RESULT_LOAD_IMAGE && resultCode == ctx.RESULT_OK && null != data) {
+
                 Uri selectedImage = data.getData();
+
                 String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
                 Cursor cursor = ctx.getContentResolver().query(selectedImage,
                         filePathColumn, null, null, null);
+
                 cursor.moveToFirst();
 
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+
                 String picturePath = cursor.getString(columnIndex);
                 cursor.close();
+
                 //imgPickPhoto.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
                 Bitmap bm = BitmapFactory.decodeFile(picturePath);
+
                 Bitmap resized = Bitmap.createScaledBitmap(Constants.cropToSquare(bm), 500, 500, true);
                 //Bitmap conv_bm = getRoundedRectBitmap(resized, 200);
+
                 imgPickPhoto.setImageBitmap(Constants.getCroppedBitmap(resized));
             }
         }catch(Exception ex){
+
             Toast.makeText(ctx, "Photo can not be loaded", Toast.LENGTH_SHORT).show();
+            
         }
     }
 
